1fa52e045421a77b09a89dd9c036e6d29639752b
DiegoYokoyama <Dih.yokoyama03@gmail.com>
2023-05-08 15:52:39 -0400

    islower e isupper
    islower fala se a variavel e minusculo se for vai ser (True) se nao for vai ser (False)
    isupper ve se a variavel e maiusculo se for vai ser (True) se nao for vai ser (False)

--- islower e isupper.py
+++ islower e isupper.py
@@ -1,4 +1,5 @@
-#islower fala se a variavel e minusculo enquanto isupper ve se a variavel e maiusculo 
+#islower fala se a variavel e minusculo se for vai ser (True) se nao for vai ser (False) 
+#isupper ve se a variavel e maiusculo se for vai ser (True) se nao for vai ser (False)
 a="DIEGO"
 print(a.islower())
 print(a.isupper())
