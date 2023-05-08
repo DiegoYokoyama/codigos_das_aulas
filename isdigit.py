83d18c6b0db3ca4009550379578b545875b0dfd4
DiegoYokoyama <Dih.yokoyama03@gmail.com>
2023-05-08 15:56:00 -0400

    isdigit
    isdigit verifica se a string so possui numeros se tiver so numeros vai ver(True) se nao tiver so numeros vai ser (false)

---
+++ isdigit.py
@@ -0,0 +1,6 @@
+#isdigit verifica se a string so possui numeros se tiver so numeros vai ver(True) se nao tiver so numeros vai ser (false)
+a="123987"
+print(a.isdigit())
+
+a="16768abc"
+print(a.isdigit())
