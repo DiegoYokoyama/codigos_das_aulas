ddb1bcf07314de1e62c6625c5a334ad825c15c78
DiegoYokoyama <Dih.yokoyama03@gmail.com>
2023-05-08 16:04:50 -0400

    replace
    Replase troca as string com oque vc quer trocar tipo (Diego Toshio) quero trocar o Toshio por (Yokoyama) ae vc coloca print(a.replace("Toshio","Yokoyama"))

---
+++ replace.py
@@ -0,0 +1,3 @@
+#Replase troca as string com oque vc quer trocar tipo (Diego Toshio) quero trocar o Toshio por (Yokoyama) ae vc coloca print(a.replace("Toshio","Yokoyama"))
+a="Diego Toshio"
+print(a.replace("Toshio","Yokoyama"))
