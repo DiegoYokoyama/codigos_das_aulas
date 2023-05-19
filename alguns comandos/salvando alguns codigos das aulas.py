65135271246c96f3390740a0dfdcc3d2a03dce9b
DiegoYokoyama <Dih.yokoyama03@gmail.com>
2023-05-19 15:12:50 -0400

    Update salvando alguns codigos das aulas.py

--- alguns comandos/salvando alguns codigos das aulas.py
+++ alguns comandos/salvando alguns codigos das aulas.py
@@ -1,11 +1,11 @@
-Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+#Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+#Type "help", "copyright", "credits" or "license()" for more information.
 print(3.14)
 3.14
 print("s")
-s
+#s
 print(type(3.14))
-<class 'float'>
+#class 'float'
 print(True)
 True
 print(3*3)
@@ -37,13 +37,13 @@ pi=3.14159
 print(pi)
 3.14159
 print((5+7),(10-3),(9*9),(7/2),(4**2),(11%2),(11//2))
-12 7 81 3.5 16 1 5
+#12 7 81 3.5 16 1 5
 print(2*(3-1))
 4
 print(2*3-1)
 5
 print((2*(3-1)),(2*3-1),(10/2*2),(10/(2*2)),(2**3**2),(2**(3**2)),((2**3)**2))
-4 5 10.0 2.5 512 512 64
+#4 5 10.0 2.5 512 512 64
 
 print("vamos estudar na", end='')
 print('senac Hub Academy')
