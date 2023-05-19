8871a1c3bd7f29f30539c7f21242b9042737eb59
DiegoYokoyama <132005322+DiegoYokoyama@users.noreply.github.com>
2023-05-18 20:38:01 -0400

    update exer teste

--- testando exercicio avaliativo.py
+++ testando exercicio avaliativo.py
@@ -3,14 +3,14 @@ print('=========================================================================
 print('                                     Bem Vindo                                  ') #coloquei um print com Boa Tarde
 print('================================================================================')#coloquei um print com (=) so pra ficar bonito
 
-num=int(input('Por favor Digite um numero para ver se e  divisível por 4, mas não por 6: '))#esse e o comando para vc digitar um numero 
-print('*Você pode ver o resultado aqui em baixo !')#coloquei esse printe para informar que o resultado vai sair em baixo dessa mensagem
+num=int(input(' Por favor Digite um numero para ver se e  divisível por 4, mas não por 6: '))#esse e o comando para vc digitar um numero 
+print('\n Você pode ver o resultado aqui em baixo !')#coloquei esse printe para informar que o resultado vai sair em baixo dessa mensagem
 if num % 4 == 0 and num % 6 != 0: #esse if e para saber se o numero digitado e divisível por 4, mas não por 6
-    print('O numero e  divisível por 4 mas não por 6: %d'%(num))#esse print vai falar se ele e divisível por 4,  mas não por 6 (coloquei o %d para mostrar o numero que eu digitei)
+    print('\n O numero e divisível por 4 mas não por 6: %d'%(num))#esse print vai falar se ele e divisível por 4,  mas não por 6 (coloquei o %d para mostrar o numero que eu digitei)
 else:#esse else vai colocar o numero que e divisível por 6
     print('o numero não atende ao criterio: %d'%(num))#esse print vai falar se ele divisível por 6 se for ele vai aparecer aqui 
 print('================================================================================')
-print('                     Obrigado volte Sempre que tiver duvida                     ')#coloquei um printe com obrigado 
+print('                              Obrigado volte Sempre                             ')#coloquei um printe com obrigado 
 print('================================================================================')
 
 #para um numero ser divisível por 4 se os 2 ultimo numero e divisível por 4 exemplo 512,100,236
