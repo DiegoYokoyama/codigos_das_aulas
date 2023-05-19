6a74e26f7381861276b2ff54cacf2eb01627de24
DiegoYokoyama <132005322+DiegoYokoyama@users.noreply.github.com>
2023-05-18 20:46:21 -0400

    Update testando exercicio avaliativo.py

--- testando exercicio avaliativo.py
+++ testando exercicio avaliativo.py
@@ -1,6 +1,6 @@
 #Atividade avaliativa para saber se o numero e divisível por 4, mas não por 6
 print('================================================================================')#coloquei um print com (=) so pra ficar bonito
-print('                                     Bem Vindo                                  ') #coloquei um print com Boa Tarde
+print('                                     Bem Vindo                                  ') #coloquei um print com bem vindo
 print('================================================================================')#coloquei um print com (=) so pra ficar bonito
 
 num=int(input(' Por favor Digite um numero para ver se e  divisível por 4, mas não por 6: '))#esse e o comando para vc digitar um numero 
