eeb2a5ee65ff16565904ace0ee27fc1d5817a7fd
DiegoYokoyama <Dih.yokoyama03@gmail.com>
2023-05-26 15:20:18 -0400

    Update testando exercicio avaliativo.py

--- testando exercicio avaliativo.py
+++ testando exercicio avaliativo.py
@@ -22,3 +22,14 @@ print('=========================================================================
 #agora alguns num que nao atende a esse criterio exemplo: 103,550,361
 #fiquei um pouco bugado nesse exercicio mais acho que e isso 
 #Um número é divisível por 6 se for par e a soma de seus algarismos for divisível por 3.
+
+while True:
+        op=input(' 1 - digite 1 para digitar um numero\n 2 - digite 2 para saber qual é o maior numero, e o menor numero\n Digite aqui: ')
+        if op=='1':
+            num=int(input('Digite qualquer numero: '))
+            nu.append(num)
+            os.system('cls')
+        elif op=='2':
+            print('O maior numero digitado foi: ',max(nu))
+            print('O menor numero digitado foi: ',min(nu))
+            break
