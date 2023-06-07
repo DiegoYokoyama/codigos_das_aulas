499cd16c4df2f050564cacd3c258c664fd118c98
DiegoYokoyama <Dih.yokoyama03@gmail.com>
2023-06-07 17:25:34 -0400

    testando codigo da prova

---
+++ teste.py
@@ -0,0 +1,23 @@
+'''num=float(input('Digite numero: '))
+num2=float(input('Digite outro numero: '))
+soma=num+num2
+print(soma)
+
+for a in range(2,101,2):
+    print(a)
+for a in range(1,100,2):
+    print(a)
+
+while True:
+    print('Cadastro client\n Cadastro paciente\n Cadastro consulta\n relatorio\n')'''
+try:
+    num=int(input('Digite numero: '))
+    num2=int(input('Digite outro numero: '))
+    divi=num/num2
+except:
+    print('um numero inteiro')
+
+
+
+
+
