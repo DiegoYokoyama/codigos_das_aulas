8d01cf09b5e0b103768198a1a3c825cfb4c1cda3
DiegoYokoyama <Dih.yokoyama03@gmail.com>
2023-06-27 14:09:09 -0400

    re

--- print.py
+++ print.py
@@ -1,24 +1,39 @@
-import pprint
-data = {'name': 'John', 'age': 30, 'city': 'New York'}
+import re
 
-pprint.pprint(data)
-#--------------------------------------------------------------
-data = {'name': 'John', 'age': 30, 'city': 'New York'}
+# Texto de exemplo
+texto = "O gato preto cruzou o caminho do cachorro preto."
+# Padrão de expressão regular para substituir palavras
+padrao_substituicao = r"preto"
+# Substituir todas as ocorrências da palavra "preto" por "branco"
+texto_substituido = re.sub(padrao_substituicao, "branco", texto)
+print("Texto original:", texto)
+print("Texto substituído:", texto_substituido)
+#------------------------------------------------------
 
-formatted_data = pprint.pformat(data)
-print(formatted_data)
-#--------------------------------------------------------------
-data = {'name': 'John', 'age': 30, 'city': 'New York'}
+# Lista de endereços de e-mail de exemplo
+emails = [
+    "usuario@example.com",
+    "usuario@dominio.com.br",
+    "meu.email@empresa.co",
+    "contato@123.abc",
+    "email.invalido",
+]
+# Padrão de expressão regular para validar endereços de e-mail
+padrao_email = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+# Verificar se os endereços de e-mail são válidos
+for email in emails:
+    if re.match(padrao_email, email):
+        print(f"O endereço de e-mail '{email}' é válido.")
+    else:
+        print(f"O endereço de e-mail '{email}' é inválido.")
+#------------------------------------------------------------------------
 
-readable = pprint.isreadable(pprint.pformat(data))
-print(readable)
-#--------------------------------------------------------------
-data = {'name': 'John', 'age': 30, 'city': 'New York'}
-data['self'] = data
-
-recursive = pprint.isrecursive(data)
-print(recursive)
-#--------------------------------------------------------------
-
-#https://docs.python.org/3/library/pprint.html
-#https://chat.openai.com/c/ee456ace-9b38-44dd-a914-ec4f2a556d96
+# Texto de exemplo
+texto = "Olá! Meu número de telefone é (123) 456-7890. Por favor, me ligue."
+# Padrão de expressão regular para encontrar números de telefone
+padrao_telefone = r"\(\d{3}\) \d{3}-\d{4}"
+# Procurar por números de telefone no texto
+numeros_telefone = re.findall(padrao_telefone, texto)
+# Imprimir os números de telefone encontrados
+for numero in numeros_telefone:
+    print("Número de telefone encontrado:", numero)
