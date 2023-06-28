ce5ac99eacbad1ac77ce194fc895855e8baddfa1
DiegoYokoyama <132005322+DiegoYokoyama@users.noreply.github.com>
2023-06-27 20:41:52 -0400

    Update print.py

--- print.py
+++ print.py
@@ -7,3 +7,7 @@ if match:
     print('correspondencia encontrada',match.group())
 else:
     print('nenhuma correspondencia encontrada')
+
+    #A re.search()função procura um padrão dentro de uma string e retorna um objeto de correspondência se uma correspondência for encontrada.
+    #exemplo se na pattern tiver (oi ) e eu escrever na string(oi bem vindo) vai pegar so o (oi ) pq na pattern so tem o (oi)
+    #outro exemplo se na pattern tiver (ola) e na string tiver so (oi) nao vai retornar nada pq nao tem nenhuma palavra igual
