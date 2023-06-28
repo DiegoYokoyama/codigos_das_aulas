89d9d9e413aa5d892fce763e670511c25f32549c
DiegoYokoyama <132005322+DiegoYokoyama@users.noreply.github.com>
2023-06-27 20:24:50 -0400

    Update print.py

--- print.py
+++ print.py
@@ -1,29 +1,9 @@
 import re
 
-# Texto de exemplo
-texto = "O gato preto cruzou o caminho do cachorro preto."
-# Padrão de expressão regular para substituir palavras
-padrao_substituicao = r"preto"
-# Substituir todas as ocorrências da palavra "preto" por "branco"
-texto_substituido = re.sub(padrao_substituicao, "branco", texto)
-print("Texto original:", texto)
-print("Texto substituído:", texto_substituido)
-#------------------------------------------------------
-
-# Lista de endereços de e-mail de exemplo
-emails = [
-    "usuario@example.com",
-    "usuario@dominio.com.br",
-    "meu.email@empresa.co",
-    "contato@123.abc",
-    "email.invalido",
-]
-# Padrão de expressão regular para validar endereços de e-mail
-padrao_email = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-# Verificar se os endereços de e-mail são válidos
-for email in emails:
-    if re.match(padrao_email, email):
-        print(f"O endereço de e-mail '{email}' é válido.")
-    else:
-        print(f"O endereço de e-mail '{email}' é inválido.")
-#------------------------------------------------------------------------
+pattern=r=(input("digite algo: "))
+string=(input('digite algo que tem aver com a primeira: '))
+match=re.search(pattern,string)
+if match:
+    print('correspondencia encontrada',match.group())
+else:
+    print('nenhuma correspondencia encontrada')
