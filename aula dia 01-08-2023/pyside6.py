b7552460b881070647f5ffb90c8f1c98981136dd
DiegoYokoyama <Dih.yokoyama03@gmail.com>
2023-08-01 17:10:30 -0400

    so tirei Qlabel

--- aula dia 01-08-2023/pyside6.py
+++ aula dia 01-08-2023/pyside6.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLabel
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
 import sys
 
 class MainWindow(QMainWindow):
