59f917577ae12c89bd5caf8aabd2878dff3c589b
DiegoYokoyama <132005322+DiegoYokoyama@users.noreply.github.com>
2023-08-02 19:24:43 -0400

    area de um quadrado
    calcule a area de um quadrado em dobro

---
+++ aula dia 01-08-2023/pyside exer 8.py
@@ -0,0 +1,37 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("area de um quadrado o dobro")
+        self.setGeometry(100, 100, 300, 150)
+
+        self.label1 = QLabel("Número 1:", self)
+        self.label1.setGeometry(10, 10, 80, 30)
+
+        self.input1 = QLineEdit(self)
+        self.input1.setGeometry(100, 10, 80, 30)
+
+        self.result_label = QLabel(self)
+        self.result_label.setGeometry(10, 90, 280, 30)
+
+        self.button = QPushButton("Calcular", self)
+        self.button.setGeometry(190, 10, 100, 70)
+        self.button.clicked.connect(self.calcular_area_do_quadrado)
+
+    def calcular_area_do_quadrado(self):
+        num1 = int(self.input1.text())
+        area = num1**2
+        area_dobro = area*2
+        self.result_label.setText(f"area do quadrado : {area}\n o dobro da area: {area_dobro}")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
+
