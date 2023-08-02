d58c65875f6402fe0131424110936023b7c0d250
DiegoYokoyama <132005322+DiegoYokoyama@users.noreply.github.com>
2023-08-01 22:42:03 -0400

    exercicio completo

--- aula dia 01-08-2023/pysude exer 4.py
+++ aula dia 01-08-2023/pysude exer 4.py
@@ -1,39 +1,42 @@
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLabel, QPushButton, QLineEdit
-from PySide6.QtCore import QSize, Qt
 import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("exercicio 4")
-        self.setGeometry(100,100,300,150)
-        
-        self.label1 = QLabel('Numero 1:', self)
-        self.label1.setGeometry(10,10,80,30) 
-        
+
+        self.setWindowTitle("Soma de Números")
+        self.setGeometry(100, 100, 300, 150)
+
+        self.label1 = QLabel("Número 1:", self)
+        self.label1.setGeometry(10, 10, 80, 30)
+
         self.input1 = QLineEdit(self)
-        self.input1.setGeometry(100,10,80,30)
-        
-        self.label2 = QLabel('Numero 2:', self)
-        self.label2.setGeometry(10,50,80,30) 
-        
+        self.input1.setGeometry(100, 10, 80, 30)
+
+        self.label2 = QLabel("Número 2:", self)
+        self.label2.setGeometry(10, 50, 80, 30)
+
         self.input2 = QLineEdit(self)
-        self.input2.setGeometry(100,50,80,30)
-        
+        self.input2.setGeometry(100, 50, 80, 30)
+
         self.result_label = QLabel(self)
-        self.result_label.setGeometry(10,90,280,30)
-        
-        self.button = QPushButton('Calcular', self)
-        self.button.setGeometry(190,10,100,70)
-        
+        self.result_label.setGeometry(10, 90, 280, 30)
+
+        self.button = QPushButton("Calcular", self)
+        self.button.setGeometry(190, 10, 100, 70)
+        self.button.clicked.connect(self.calcular_soma)
+
     def calcular_soma(self):
-        num1 = int(self.input1.texto())
+        num1 = int(self.input1.text())
         num2 = int(self.input2.text())
         soma = num1 + num2
-        self.result_label.setText(f'')
-        
+        self.result_label.setText(f"A soma é: {soma}")
 
-app = QApplication(sys.argv)
-janela = MainWindow()
-janela.show()
-app.exec()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
