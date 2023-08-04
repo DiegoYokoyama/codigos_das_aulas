952a49e9c6ccf2502280b8ce47049cd4e6d5238e
DiegoYokoyama <Dih.yokoyama03@gmail.com>
2023-08-04 17:10:03 -0400

    arrumei o check box

--- aula dia 04-08-2023/exer 1.py
+++ aula dia 04-08-2023/exer 1.py
@@ -1,33 +1,46 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QCheckBox, QVBoxLayout,QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QCheckBox, QVBoxLayout, QWidget
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("CHECK BOX")
-        self.label=QLabel("CLIQUE E ACEITE É ISSO")
-        self.ck=QCheckBox("ACEITO!!!!")
-        self.label2=QLabel()
-
+        self.setWindowTitle("Check Box")
+        self.label = QLabel("Aceita casar comigo ?")
+        self.ck = QCheckBox("Aceito")
+        self.ck2 = QCheckBox("NÃO ACEITO")
+        self.label2 = QLabel()
+        
         layout = QVBoxLayout()
         layout.addWidget(self.label)
         layout.addWidget(self.ck)
         layout.addWidget(self.label2)
+        layout.addWidget(self.ck2)
         
-        container=QWidget()
+        container = QWidget()
         container.setLayout(layout)
         
         self.setCentralWidget(container)
-        self.ck.stateChanged.connect(self.state)            
-    
+        self.ck.stateChanged.connect(self.state)
+        self.ck2.stateChanged.connect(self.state2)
+        
     def state(self, s):
+        print(s)
         if s == 2:
-            self.label2.setText("NÃO!!")
+            self.label2.setText("ACEITO")
+            self.ck2.setChecked(False)
         else:
-            self.label2.setText("some daqui")    
-    
-app=QApplication(sys.argv)
-w=MainWindow()
+            self.ck2.setChecked(True)
+            
+    def state2(self, s):
+        print(s)
+        if s == 2:
+            self.label2.setText("NÃO ACEITO")
+            self.ck.setChecked(False)
+        else:
+            self.ck.setChecked(True)
+
+app = QApplication(sys.argv)
+w = MainWindow()
 w.show()
 app.exec()
---
+++ aula dia 04-08-2023/exer 2.py
@@ -0,0 +1,46 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QCheckBox, QVBoxLayout,QWidget, QLineEdit, QPushButton
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("cadastro")
+        self.setGeometry(150, 150, 500, 500)
+
+        self.label1 = QLabel("nome:", self)
+        self.label1.setGeometry(20, 5, 80, 30)
+
+        self.input1 = QLineEdit(self)
+        self.input1.setGeometry(80, 10, 410, 20)
+
+        self.label2 = QLabel("telefone:", self)
+        self.label2.setGeometry(20, 45, 80, 30)
+
+        self.input2 = QLineEdit(self)
+        self.input2.setGeometry(80, 50, 410, 20)
+        
+        self.label3 = QLabel("endereço:", self)
+        self.label3.setGeometry(20, 85, 80, 30)
+
+        self.input3 = QLineEdit(self)
+        self.input3.setGeometry(80, 90, 410, 20)
+        
+        self.label4 = QLabel("sexo:", self)
+        self.label4.setGeometry(20, 125, 80, 30)
+
+        self.button = QPushButton("cadastro", self)
+        self.button.setGeometry(5, 460, 490, 20)
+    
+    def masculino(self):
+        self.label5=QLabel("masculino",self)
+        self.label5.setGeometry(5,500,490,20)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
