cdd99acec255eafe956929d80b8ee819bd7a0f0a
DiegoYokoyama <Dih.yokoyama03@gmail.com>
2023-08-17 17:01:57 -0400

    so adicionei uma descrição

--- comandos feito MySQL/mysql netflix.sql
+++ comandos feito MySQL/mysql netflix.sql
@@ -1,14 +1,14 @@
-create database netflix character set utf8mb4 collate utf8mb4_unicode_ci;
-use netflix;
-create table usuario (
-nome varchar (50) not null,
+create database netflix character set utf8mb4 collate utf8mb4_unicode_ci; #caracteristica do banco de dados e um banco de dados que vc vai criar
+use netflix; #e o banco de dados que vc deseja usar
+create table usuario ( #vai criar uma tabela
+nome varchar (50) not null,  #e um nome que vc vai criar dentro da tabela varchar e um caracter 
 cpf varchar(14) not null, 
 endereco varchar (50) not null, 
 email varchar (40) unique, 
 senha varchar (50) not null,
 telefone varchar (15) not null,
 cartao varchar (20) not null,
-primary key (cpf));
+primary key (cpf)); # primery key e um numero unico que nao repeti
 
 create table mensalidade (
 mes_ano date not null,
@@ -31,3 +31,6 @@ duracao float not null,
 produtora varchar (50) not null,
 tipo varchar (50) not null,
 primary key (titulo));
+
+rename table video2 to video; #vai alterar o nome da tabela
+alter table video change temporada2 temporada int  #vai alterar dentro da tabela que vc escolheu
