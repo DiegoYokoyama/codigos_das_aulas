37165231e17c9e2524be293b9f57450323edd809
DiegoYokoyama <Dih.yokoyama03@gmail.com>
2023-08-25 15:13:08 -0400

    testando o comando pyinstaller

--- algumus exercicio teste das aulas antigas/exercicio antigos/teste.py
+++ algumus exercicio teste das aulas antigas/exercicio antigos/teste.py
@@ -17,7 +17,15 @@ try:
 except:
     print('um numero inteiro')
 
+'''print("vamos estudar na", end='')
+print('senac Hub Academy')
+print('1,2,3')
+print('dia','mes','ano',sep='/')
+print('ontem','hoje','amanha',sep='-')
+print("B","n","n",".",sep='a')
+print(81**0.5)
 
+'''
 
 
 
--- algumus exercicio teste das aulas antigas/exercicio antigos/testes.py
+++
@@ -1,8 +0,0 @@
-'''print("vamos estudar na", end='')
-print('senac Hub Academy')
-print('1,2,3')
-print('dia','mes','ano',sep='/')
-print('ontem','hoje','amanha',sep='-')
-print("B","n","n",".",sep='a')
-print(81**0.5)'''
-
