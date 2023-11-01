e4b5790d131b1352ada932263fb62dbf97870191
DiegoYokoyama <Dih.yokoyama03@gmail.com>
2023-11-01 17:17:41 -0400

    Update conecaobanco.py

--- conecaobanco.py
+++ conecaobanco.py
@@ -8,3 +8,18 @@ database = "hub_innovation",
 user = "Toshio",
 password = "DiegoToshio.2003")
 
+if con.is_connected():
+    hub_innovation = con.get_server_info()
+    print("conectado ao banco de dados =",hub_innovation)
+
+
+    consulta_sql = "select *from palestras"
+    cursor = con.cursor()
+    cursor.execute(consulta_sql)
+    linhas = cursor.fetchall()
+    print("numero total de registros retornados:", cursor.rowcount)
+    
+    print('Mostrando os Registros')
+    for linha in linhas:
+        print("palestraID = ",linha[0])
+        print("palestraTema= ", linha[1])
